c2c98aa00861645350fa73fde334d6ae087c74d4
O-Faraday <150429569+O-Faraday@users.noreply.github.com>
2024-01-18 11:17:50 +0100

    Update 0_Animation_Demo.py

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -23,42 +23,22 @@ header = lines.pop(0)
 l_params = [line.strip().split('\t')[0] for line in lines]
 l_types = [line.strip().split('\t')[1] for line in lines]
 
+# On garde que les floats pour commencer
+keep_types = ['float']
+l_keep_params = [p  for p, t in zip(l_params, l_types) if t in keep_types]
+
+# 
+# 2- Selection des paramètres que l'on souhaite afficher
+selected_params = st.sidebar.multiselect("Choisir les paramètres à affichers :", l_keep_params)
+
+# 3- Affichage des paramètres au cours du temps
+for param in selected_params :
+    file_path = 'f"{data_dir}/csv/{param}.csv.zip'
+    data = pd.read_csv(file_path, index_col="Unnamed: 0")
+    data["indice"] =data.index
+    # Afficher le DataFrame dans la page centrale
+    st.title("DataFrame")
+    st.write(data)
 
 
-file_param_path = 'data/Parameter_Datatype.txt'
-data = pd.read_csv(file_path, index_col="Unnamed: 0")
-data["indice"] =data.index
 
-
-# Convertir les colonnes spécifiées en entiers
-data = data.replace(',', '.', regex=True)
-data = data.rename(columns=lambda x: x.split(' [')[0])
-
-# Sidebar
-st.sidebar.title("Paramètres du graphe")
-feature_col = st.sidebar.selectbox("Abscisse :", data.columns)
-target_col = st.sidebar.selectbox("Ordonnée :", data.columns)
-color_col = st.sidebar.selectbox("Couleur :", data.columns)
-
-# Personnaliser les bulles d'informations
-hover_cols = st.sidebar.multiselect("Choisir les colonnes pour les bulles d'infos :", data.columns)
-hover_cols = [feature_col, target_col, color_col] + hover_cols  
-
-# Afficher le DataFrame dans la page centrale
-st.title("DataFrame")
-st.write(data)
-
-# Créer un scatter plot avec Altair
-st.title("Representation des jeux de train et de test")
-
-# Votre code Altair pour le graphique de dispersion
-scatter_chart = alt.Chart(data).mark_circle().encode(
-    x=alt.X(feature_col),  # Fixer l'origine en bas à gauche
-    y=alt.Y(target_col),  # Fixer l'origine en bas à gauche
-    color=color_col,
-    tooltip=list(hover_cols)
-).interactive()
-
-
-# Afficher le plot dans la page centrale
-selected_point = st.altair_chart(scatter_chart, use_container_width=True)
