64efc64febb376efecf0f77305a862d5f8929d9c
O-Faraday <150429569+O-Faraday@users.noreply.github.com>
2024-02-02 11:36:27 +0100

    Update start.py

--- start.py
+++ start.py
@@ -20,11 +20,11 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="Alert",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Julien's data! 👋")
+    st.write("# Welcome to Alert ! 👋")
 
     st.sidebar.success("Select a demo above.")
 
